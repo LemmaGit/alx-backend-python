194acbadde2786546562fd09a1447c8793b0128e
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-07-27 14:42:21 +0300

    updated url files

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,27 +1,29 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
 from rest_framework.response import Response
-from rest_framework.decorators import action
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-from django.shortcuts import get_object_or_404
-
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__email']  # example filter
 
     def perform_create(self, serializer):
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+        return Response(self.get_serializer(conversation).data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
+    ordering = ['-sent_at']
 
     def perform_create(self, serializer):
         serializer.save(sender=self.request.user)
-
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
