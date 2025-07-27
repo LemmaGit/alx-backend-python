b00535ac8e90fc6149db315ac67421de6ccb23a8
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-07-27 14:34:14 +0300

    updated the serializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User, Conversation, Message
+from rest_framework.exceptions import ValidationError
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,10 +15,24 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
 
+
+class CustomSerializer(serializers.Serializer):
+    example_field = serializers.CharField()
+
+    def validate_example_field(self, value):
+        if not value.isalpha():
+            raise ValidationError("Only letters are allowed.")
+        return value
+
+
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        return MessageSerializer(obj.messages.all(), many=True).data
+
