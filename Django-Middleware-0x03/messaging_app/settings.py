45a25340361cc7fd8338fd0164ff76eb9ecad79a
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-08-03 13:09:41 +0300

    renamed a dir test

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-     'rest_framework_simplejwt',
-     'django_filters',
+    'rest_framework_simplejwt',
+    'django_filters',
     'chats',
 ]
 
