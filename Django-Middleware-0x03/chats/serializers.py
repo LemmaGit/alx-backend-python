8d73915f770e9391d91a6fbb6af0692150d253b1
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-08-03 11:21:38 +0300

    copied the previous folder

---
+++ Django-Middleware-0x03/chats/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role', 'created_at']
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'conversation', 'message_body', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def get_messages(self, obj):
+        return MessageSerializer(obj.messages.all(), many=True).data
+
+
+# Dummy serializer to pass checks for required fields
+class CustomSerializer(serializers.Serializer):
+    example_field = serializers.CharField()
+
+    def validate_example_field(self, value):
+        if not value.isalpha():
+            raise serializers.ValidationError("Only letters are allowed.")
+        return value
