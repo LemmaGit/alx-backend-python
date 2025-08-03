8630a8c82241bb9122b9b8c847c3d752d5dc30da
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-08-03 22:08:34 +0300

    added a model for a Message history

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -15,3 +15,9 @@ class Notification(models.Model):
     is_read = models.BooleanField(default=False)
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="history")
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.CASCADE)
