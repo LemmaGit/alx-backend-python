cddc59802f363e234e97b3b68a48f121a0b8031b
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-08-03 22:23:34 +0300

    added ORM Manager for Unread Messages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
+
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_messages")
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_messages")
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
     parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
 
     def get_all_replies(self):
@@ -18,6 +23,8 @@ class Message(models.Model):
                 fetch_children(child)
         fetch_children(self)
         return replies
+    objects = models.Manager() 
+    unread = UnreadMessagesManager()
     
 
 class Notification(models.Model):
