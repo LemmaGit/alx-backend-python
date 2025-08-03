d6bca81d86b97b3dec73d26ac81e30aadf39225c
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-08-03 11:51:38 +0300

    added a middelware that Restrict Chat Access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -14,4 +15,17 @@ class RequestLoggingMiddleware:
         user = request.user if request.user.is_authenticated else 'Anonymous'
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
         logging.info(log_message)
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        # Deny access if current time is between 21 (9 PM) and 6 AM
+        if current_hour >= 21 or current_hour < 6:
+            return HttpResponseForbidden("Access to the messaging app is restricted between 9 PM and 6 AM.")
+
         return self.get_response(request)
